e619f90f43940e43d3a199f4f479c6ba246d94cd
Laiipj <160990405+futureproject971@users.noreply.github.com>
2026-09-19 15:32:17 +0000

    hero: simplificar HUD lateral para composicao da referencia

--- src/components/crazy-hero/HudDecoration.tsx
+++ src/components/crazy-hero/HudDecoration.tsx
@@ -2,16 +2,16 @@ export function HudDecoration() {
   return (
     <div className="crazy-hud" aria-hidden="true">
       <div className="crazy-hud__copy crazy-hud__copy--tl">
-        <span>GAMES</span><span>SOFTWARES</span><span>PERFORMANCE</span><span>SEM LIMITES</span>
-      </div>
-      <div className="crazy-hud__copy crazy-hud__copy--ml">
-        <span>PLAY</span><span>OPTIMIZE</span><span>EVOLVE</span>
-      </div>
-      <div className="crazy-hud__copy crazy-hud__copy--tr">
-        <span>TECNOLOGIA</span><span>DE VERDADE</span><span>PARA GAMERS</span><span>REAIS</span>
+        <span>GAMES</span>
+        <span>PERFORMANCE</span>
+        <span>LIBERDADE</span>
+        <span>SEM LIMITES</span>
       </div>
       <div className="crazy-hud__copy crazy-hud__copy--br">
-        <span>MAIS JOGOS</span><span>MAIS LIBERDADE</span>
+        <span>JOGUE</span>
+        <span>MAIS</span>
+        <span>VIVA</span>
+        <span>LIVRE</span>
       </div>
       <div className="crazy-hud__line crazy-hud__line--left" />
       <div className="crazy-hud__line crazy-hud__line--right" />
